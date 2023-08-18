29f4cfdc6195c38ff0cf67668d6b8d87c7a39034
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-08-18 20:08:55 +0200

    worked on IWA15 but did not complete

---
+++ IWA15/script.js
@@ -0,0 +1,50 @@
+
+const data = {
+	lists: [
+		['first', [15, 11, 13, 7, 5]],
+		['second', [2, 6, 8, 4, 14, 12, 10]],
+		['third', [9, 3, 1]],
+	]
+}
+
+// Only edit below
+
+const {lists:[first]} = data
+const { lists:[second] } = data
+const { lists:[third]} = data
+
+const result = []
+
+const extractBiggest = () => {
+	if (first[-1] > second[-1]) {
+		return first
+	}
+
+	if (third[-1] < 1) {
+		return second
+	}
+	
+	return third
+}
+
+// Only edit above
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+result.push(extractBiggest())
+
+console.log(result)
